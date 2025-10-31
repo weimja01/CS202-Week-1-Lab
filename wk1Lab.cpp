3535d1e587f9c0ef4e8e3b37447d006b0f108569
weimja01 <weimja01@yahoo.com>
2025-10-30 20:47:36 -0400

    final

---
+++ wk1Lab.cpp
@@ -0,0 +1,24 @@
+/* Program name: wk1Lab.cpp
+* Author: Jeff Weimer
+* Date last updated: 10/30/2025
+* Purpose: intro
+*/
+#include <iostream>
+
+void prompt();
+
+int main (){
+
+    prompt();
+
+    return 0;
+}
+
+void prompt()
+{
+    std::string line;
+    std::cout << "Enter a statement to finish the sentence" << std::endl;
+    std::cout << "Welcome to:" << std:: endl;
+    std::getline(std::cin, line);
+    std::cout << "Welcome to " << line << "!" << std::endl;
+}
